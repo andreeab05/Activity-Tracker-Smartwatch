6bd6a099df4bfc4783a41202069400dd4c7b20aa
andreea <andreea.borbei@stud.acs.upb.ro>
2024-04-05 11:03:24 +0300

    Created functionality to send UID from smartwatch to phone app

--- app/src/main/java/com/example/activitytrackerwearos/presentation/MainActivity.kt
+++ app/src/main/java/com/example/activitytrackerwearos/presentation/MainActivity.kt
@@ -6,68 +6,123 @@
 
 package com.example.activitytrackerwearos.presentation
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TimeText
-import com.example.activitytrackerwearos.R
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.activitytrackerwearos.presentation.theme.ActivityTrackerWearOSTheme
+import android.provider.Settings
+import android.util.Log
+import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.TimeText
+import androidx.wear.compose.material.Vignette
+import androidx.wear.compose.material.VignettePosition
+import com.example.activitytrackerwearos.presentation.screens.PhonePairingScreen
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.android.gms.wearable.CapabilityClient
+import com.google.android.gms.wearable.CapabilityInfo
+import com.google.android.gms.wearable.Node
+import com.google.android.gms.wearable.Wearable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val MESSAGE_PASSING_CAPABILITY_NAME = "message_passing"
+    private val MESSAGE_PASSING_MESSAGE_PATH = "/message_passing"
+    private var transcriptionNodeId: String? = null
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-
         super.onCreate(savedInstanceState)
 
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-            WearApp("Android")
+            WearApp { setupMessagePassing() }
+        }
+    }
+
+    private fun getDeviceId(context: Context): String {
+        return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+    }
+
+    private val capabilityListener = CapabilityClient.OnCapabilityChangedListener { capabilityInfo ->
+        // Handle capability changes here
+    }
+
+    private fun setupMessagePassing() {
+        if (applicationContext == null)
+            return
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val capabilityInfo: CapabilityInfo = Tasks.await(
+                    Wearable.getCapabilityClient(applicationContext)
+                        .getCapability(
+                            MESSAGE_PASSING_CAPABILITY_NAME,
+                            CapabilityClient.FILTER_REACHABLE
+                        )
+                )
+                // capabilityInfo has the reachable nodes with the transcription capability
+                updateTranscriptionCapability(capabilityInfo).also {
+                    Wearable.getCapabilityClient(applicationContext).addListener(
+                        capabilityListener,
+                        MESSAGE_PASSING_CAPABILITY_NAME
+                    )
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun updateTranscriptionCapability(capabilityInfo: CapabilityInfo) {
+        transcriptionNodeId = pickBestNodeId(capabilityInfo.nodes)
+        val deviceId = getDeviceId(this)
+        requestSendingUID(deviceId.toByteArray())
+        //Log.d("node", "$transcriptionNodeId")
+    }
+
+    private fun pickBestNodeId(nodes: Set<Node>): String {
+        // Find a nearby node or pick one arbitrarily.
+        return nodes.first().id
+    }
+
+
+    private fun requestSendingUID(uid: ByteArray) {
+        transcriptionNodeId?.also { nodeId ->
+            val sendTask: Task<*> = Wearable.getMessageClient(applicationContext).sendMessage(
+                nodeId,
+                MESSAGE_PASSING_MESSAGE_PATH,
+                uid
+            ).apply {
+                addOnSuccessListener { Log.d("msg_passing", "Message sent successfully") }
+                addOnFailureListener { exception -> Log.d("msg_passing", "Failed $exception") }
+            }
         }
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun WearApp(sendUID: () -> Unit) {
+
     ActivityTrackerWearOSTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            contentAlignment = Alignment.Center
+        Scaffold(
+            timeText = {
+                TimeText()
+            },
+            vignette = {
+                Vignette(vignettePosition = VignettePosition.TopAndBottom)
+            },
         ) {
-            TimeText()
-            Greeting(greetingName = greetingName)
+            PhonePairingScreen(sendUID = sendUID)
         }
     }
 }
 
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
-
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
-}
