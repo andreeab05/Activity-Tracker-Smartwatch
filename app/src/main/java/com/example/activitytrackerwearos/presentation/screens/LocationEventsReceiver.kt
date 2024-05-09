a4e756bfe36889731eb9dcc50dbd7908a7dd3666
andreea <andreea.borbei@stud.acs.upb.ro>
2024-05-10 01:50:42 +0300

    Implemented broadcast receiver for location events

--- app/src/main/java/com/example/activitytrackerwearos/presentation/screens/LocationEventsReceiver.kt
+++ app/src/main/java/com/example/activitytrackerwearos/presentation/screens/LocationEventsReceiver.kt
@@ -1,4 +1,16 @@
 package com.example.activitytrackerwearos.presentation.screens
 
-class LocationEventsReceiver {
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class LocationEventsReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if(intent.action == "com.example.NEW_REPEATING_ALARM"){
+            Log.d("broadcast", "blabla")
+        }
+
+    }
 }
