daa15b040f169417535677129e86642936368bea
andreea <andreea.borbei@stud.acs.upb.ro>
2024-05-13 21:51:34 +0300

    Added custom colors

--- app/src/main/java/com/example/activitytrackerwearos/presentation/theme/Colors.kt
+++ app/src/main/java/com/example/activitytrackerwearos/presentation/theme/Colors.kt
@@ -1,2 +1,6 @@
 package com.example.activitytrackerwearos.presentation.theme
 
+import androidx.compose.ui.graphics.Color
+
+val DarkerLightRed = Color(0xFFD43F3F)
+val DarkerLightOrange = Color(0xFFFFB74D)
