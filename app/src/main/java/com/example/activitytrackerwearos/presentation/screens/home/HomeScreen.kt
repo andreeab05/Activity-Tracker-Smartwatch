1dd7508dc105514af7f4a7a810c7236fc4f0a6a1
andreea <andreea.borbei@stud.acs.upb.ro>
2024-05-13 21:51:05 +0300

    Implemented screens

--- app/src/main/java/com/example/activitytrackerwearos/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/activitytrackerwearos/presentation/screens/home/HomeScreen.kt
@@ -1,2 +1,24 @@
 package com.example.activitytrackerwearos.presentation.screens.home
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.TimeText
+import androidx.wear.compose.material.Vignette
+import androidx.wear.compose.material.VignettePosition
+import com.example.activitytrackerwearos.presentation.theme.ActivityTrackerWearOSTheme
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    Column {
+        Text(text = "Home")
+        Log.d("Home", "we're home!!")
+    }
+}
--- app/src/main/java/com/example/activitytrackerwearos/presentation/screens/pairing/PhonePairingScreen.kt
+++ app/src/main/java/com/example/activitytrackerwearos/presentation/screens/pairing/PhonePairingScreen.kt
@@ -1,34 +1,44 @@
-package com.example.activitytrackerwearos.presentation.screens
+package com.example.activitytrackerwearos.presentation.screens.pairing
 
+import android.widget.Toast
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.NotListedLocation
 import androidx.compose.material.icons.automirrored.filled.SendToMobile
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
-import com.example.activitytrackerwearos.presentation.screens.reminders.MedicationReminderState
-import com.example.activitytrackerwearos.presentation.screens.reminders.ReminderScheduler
+import com.example.activitytrackerwearos.presentation.Graph
 
 @Composable
 fun PhonePairingScreen(
     modifier: Modifier = Modifier,
     sendUID: () -> Unit,
     setLocationMonitor: () -> Unit,
-    //medicationReminders: List<MedicationReminderState>
+    viewModel: PairingViewModel,
+    navController: NavController,
+    activity: ComponentActivity
 ) {
-    val reminderScheduler = ReminderScheduler(LocalContext.current)
+    val pairedStatus by remember {
+        viewModel.pairedState
+    }
+    var buttonPressed by remember {mutableStateOf(false)}
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -41,18 +51,31 @@ fun PhonePairingScreen(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Connect to phone app")
+            Text(
+                text = "Connect to phone app",
+                modifier = Modifier.wrapContentWidth(),
+                maxLines = 4
+            )
             Button(
                 modifier = Modifier.padding(8.dp),
-                onClick = { sendUID() },
+                onClick = {
+                    sendUID()
+                    viewModel.updatePairingStatus()
+                    buttonPressed = true
+                },
             ) {
                 Icon(Icons.AutoMirrored.Filled.SendToMobile, contentDescription = null)
             }
-            Button(
-                modifier = Modifier.padding(8.dp),
-                onClick = { reminderScheduler.schedule("Aspenter", 2, 15, 35, 2)}//setLocationMonitor()},
-            ) {
-                Icon(Icons.AutoMirrored.Filled.NotListedLocation, contentDescription = null)
+            if(buttonPressed) {
+                if (pairedStatus) {
+                    navController.popBackStack()
+                    navController.navigate(Graph.HOME)
+                } else {
+                    Toast.makeText(
+                        activity, "Try pairing again!",
+                        Toast.LENGTH_SHORT
+                    ).show();
+                }
             }
         }
     }
